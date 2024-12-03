a61f71d13e446bca045c42405c0da0ef117ae84f
Mbogo-anonymous <mbogoalex3@gmail.com>
2024-12-03 10:31:05 +0300

    initial commit

---
+++ src/main/java/com/escrow/wazipay/WazipayApplication.java
@@ -0,0 +1,13 @@
+package com.escrow.wazipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WazipayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WazipayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/escrow/wazipay/WazipayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escrow.wazipay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WazipayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
